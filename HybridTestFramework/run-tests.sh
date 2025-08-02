a49ff843b3dbb2290e1b9bbc34547df25da9acfd
kranthpothuganti <kranth.pothuganti@gmail.com>
2025-08-02 14:28:15 +0530

    .

--- HybridTestFramework/run-tests.sh
+++ HybridTestFramework/run-tests.sh
@@ -9,7 +9,7 @@ Xvfb :99 -screen 0 1920x1080x24 & export DISPLAY=:99
 
 # Run the tests by tag
 dotnet test HybridTestFramework.sln \
-    --filter TestCategory=$TAG \
+    --filter Category=$TAG \
     --logger:"trx;LogFileName=${TAG}_results.trx" \
     --results-directory "$REPORT_DIR" \
     --no-build
